df316d58a212f2e33f2f6ac47ea1bbeddb148224
mgmathobela2000 <mgmathobela2000@gmail.com>
2024-09-26 11:54:00 +0200

    Add project files.

---
+++ Loops Assignment/BeerOrMilkSongClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_Assignment
+{
+    public class BeerOrMilkSongClass
+    {
+        public string Song() {
+            //variable declaration
+            string firstSentence, middleSentence, thirdSentence, combinition;
+
+            //variable assignment
+            firstSentence = "of beer on the wall";
+            middleSentence = "of beer";
+            thirdSentence = "Take one down and pass it around";
+
+            //iterator
+            for (int a = 99; a >= 1; a = a - 1)
+            {
+                //conditional statements
+                if (a <= 99 && a != 1)
+                {
+                    //output
+                    Console.WriteLine(a + " bottles " + firstSentence + ", " + a + " bottles " + middleSentence + ". " + thirdSentence + ",");
+                    Console.WriteLine();
+                }
+                else if ( a == 1)
+                {
+                    //output
+                    Console.WriteLine(a + " bottle " + firstSentence + ", " + a + " bottle " + middleSentence + ". " + thirdSentence + ".");
+                    Console.WriteLine();
+                }
+            }
+            return combinition = firstSentence + middleSentence + thirdSentence;
+        }        
+    }
+}
---
+++ Loops Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Loops_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instance of the class
+            BeerOrMilkSongClass BMSongClass = new BeerOrMilkSongClass();
+
+            //accessing the method using the instance
+            BMSongClass.Song();
+        }
+    }
+}
